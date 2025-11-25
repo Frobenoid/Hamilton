54364a07d505568b6c6f0df501d2ca99aa70f6ee
MiltMont <miltgonmont@gmail.com>
2025-11-24 19:50:20 -0600

    Safe unwrap

--- Hamilton/Graph/Views/GraphCanvas.swift
+++ Hamilton/Graph/Views/GraphCanvas.swift
@@ -21,45 +21,49 @@ struct GraphCanvas: View {
             .overlayPreferenceValue(SocketAnchorKey.self) { anchors in
 
                 ForEach(graph.edges, id: \.id) { edge in
-                    let source = anchors[
-                        SocketAnchor(
-                            nodeID: edge.sourceNode,
-                            socketID: edge.sourceSocket,
-                            isOutput: true
-                        )
-                    ]!
 
-                    let destination = anchors[
-                        SocketAnchor(
-                            nodeID: edge.destinationNode,
-                            socketID: edge.destinationSocket
-                        )
-                    ]!
-
-                    let start = geo[source]
-                    let end = geo[destination]
-
-                    let path = self.computePath(start: start, end: end)
-
-                    path
-                        .stroke(
-                            Color.black.opacity(0.8),
-                            style: StrokeStyle(
-                                lineWidth: 5,
-                                lineCap: .round,
-                                lineJoin: .round,
+                    if let
+                        source = anchors[
+                            SocketAnchor(
+                                nodeID: edge.sourceNode,
+                                socketID: edge.sourceSocket,
+                                isOutput: true
                             )
-                        )
-                        .contentShape(
-                            path.strokedPath(StrokeStyle(lineWidth: 15))
-                        )
-                        .gesture(
-                            TapGesture(count: 2).onEnded({
-                                _ in
-                                graph.disconnect(edge.id)
-                                try! Evaluator(graph: graph).evaluate()
-                            })
-                        )
+                        ],
+
+                        let destination = anchors[
+                            SocketAnchor(
+                                nodeID: edge.destinationNode,
+                                socketID: edge.destinationSocket
+                            )
+                        ]
+                    {
+                        let start = geo[source]
+                        let end = geo[destination]
+
+                        let path = self.computePath(start: start, end: end)
+
+                        path
+                            .stroke(
+                                Color.black.opacity(0.8),
+                                style: StrokeStyle(
+                                    lineWidth: 5,
+                                    lineCap: .round,
+                                    lineJoin: .round,
+                                )
+                            )
+                            .contentShape(
+                                path.strokedPath(StrokeStyle(lineWidth: 15))
+                            )
+                            .gesture(
+                                TapGesture(count: 2).onEnded({
+                                    _ in
+                                    graph.disconnect(edge.id)
+                                    try! Evaluator(graph: graph).evaluate()
+                                })
+                            )
+                    }
+
                 }
             }
         }
