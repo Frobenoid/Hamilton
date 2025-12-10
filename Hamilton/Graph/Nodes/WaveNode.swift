0b59013478da7b0faa10a3c004721d607f71eaf8
Milton Montiel <59279510+MiltMont@users.noreply.github.com>
2025-12-09 20:48:58 -0600

    Rendering improvements (#10)
    * Uint vector implementatin
    
    * Wave node extension
    
    * Shader improvements

--- Hamilton/Graph/Nodes/WaveNode.swift
+++ Hamilton/Graph/Nodes/WaveNode.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class WaveNode : Node {
+class WaveNode: Node {
     override init() {
         super.init()
         label = "Wave"
@@ -16,17 +16,45 @@ class WaveNode : Node {
                 .withLabel("Input")
                 .withDefaultValue(0.0)
         )
-        
+
+        addInput(
+            Input<Float>()
+                .withLabel("Frequency")
+                .withDefaultValue(1.0)
+                .asUserModifiable()
+        )
+
+        addInput(
+            Input<Float>()
+                .withLabel("Amplitude")
+                .withDefaultValue(1.0)
+                .asUserModifiable()
+        )
+
+        addInput(
+            Input<Bool>()
+                .withLabel("Y-Offset")
+                .withDefaultValue(true)
+                .asUserModifiable()
+        )
+
         addOutput(
             Output<Float>()
                 .withLabel("Output")
                 .withDefaultValue(0.0)
         )
     }
-    
+
     override func execute() throws {
         let input = inputs[0].untypedCurrentValue() as! Float
-        
-        try outputs[0].setUntypedCurrentValue(to: sin(input))
+        let frequency = inputs[1].untypedCurrentValue() as! Float
+        let amplitude = inputs[2].untypedCurrentValue() as! Float
+        let shouldYOffset = inputs[3].untypedCurrentValue() as! Bool
+
+        try outputs[0].setUntypedCurrentValue(
+            to:
+                amplitude * sin(input * frequency)
+                + (shouldYOffset ? amplitude : 0)
+        )
     }
 }
