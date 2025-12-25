679118b1fa33998639c4735fbc4e94fca8917d73
MiltMont <miltgonmont@gmail.com>
2025-12-25 15:24:07 -0600

    Refactor edges

--- Hamilton/Graph/Views/GraphCanvas.swift
+++ Hamilton/Graph/Views/GraphCanvas.swift
@@ -7,6 +7,59 @@
 
 import SwiftUI
 
+struct EdgesView: View {
+    var anchors: SocketAnchorKey.Value
+    var geometryProxy: GeometryProxy
+    @Environment(Graph.self) var graph: Graph
+
+    var body: some View {
+        ForEach(graph.edges, id: \.id) { edge in
+
+            if let
+                source = anchors[
+                    SocketAnchor(
+                        nodeID: edge.sourceNode,
+                        socketID: edge.sourceSocket,
+                        isOutput: true
+                    )
+                ],
+
+                let destination = anchors[
+                    SocketAnchor(
+                        nodeID: edge.destinationNode,
+                        socketID: edge.destinationSocket
+                    )
+                ]
+            {
+                let start = geometryProxy[source]
+                let end = geometryProxy[destination]
+
+                EdgeShape(start: start, end: end)
+                    .stroke(
+                        Color.background.opacity(0.8),
+                        style: StrokeStyle(
+                            lineWidth: 5,
+                            lineCap: .round,
+                            lineJoin: .round
+                        )
+                    )
+                    .contentShape(
+                        EdgeShape(start: start, end: end)
+                            .stroke(lineWidth: 15)
+                    )
+                    .gesture(
+                        TapGesture(count: 2).onEnded({
+                            _ in
+                            graph.disconnect(edge.id)
+                            try! Evaluator(graph: graph).evaluate()
+                        })
+                    )
+            }
+
+        }
+    }
+}
+
 struct GraphCanvasView: View {
     @Environment(Graph.self) var graph: Graph
     var body: some View {
@@ -24,51 +77,10 @@ struct GraphCanvasView: View {
                 .offset(geo.size / 2)
             }
             .overlayPreferenceValue(SocketAnchorKey.self) { anchors in
-
-                ForEach(graph.edges, id: \.id) { edge in
-
-                    if let
-                        source = anchors[
-                            SocketAnchor(
-                                nodeID: edge.sourceNode,
-                                socketID: edge.sourceSocket,
-                                isOutput: true
-                            )
-                        ],
-
-                        let destination = anchors[
-                            SocketAnchor(
-                                nodeID: edge.destinationNode,
-                                socketID: edge.destinationSocket
-                            )
-                        ]
-                    {
-                        let start = geo[source]
-                        let end = geo[destination]
-
-                        EdgeShape(start: start, end: end)
-                            .stroke(
-                                Color.background.opacity(0.8),
-                                style: StrokeStyle(
-                                    lineWidth: 5,
-                                    lineCap: .round,
-                                    lineJoin: .round
-                                )
-                            )
-                            .contentShape(
-                                EdgeShape(start: start, end: end)
-                                    .stroke(lineWidth: 15)
-                            )
-                            .gesture(
-                                TapGesture(count: 2).onEnded({
-                                    _ in
-                                    graph.disconnect(edge.id)
-                                    try! Evaluator(graph: graph).evaluate()
-                                })
-                            )
-                    }
-
-                }
+                EdgesView(
+                    anchors: anchors,
+                    geometryProxy: geo
+                )
             }
         }
     }
